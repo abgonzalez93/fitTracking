86fe7b92ff752aeb6e9e340a5a27525c91f16980
Antonio <antoniomacadam2013@gmail.com>
2023-07-25 00:48:24 +0200

    Correción error Promesa

--- Backend/src/utils/processSignals.ts
+++ Backend/src/utils/processSignals.ts
@@ -1,8 +1,9 @@
-import { getDatabaseMessages } from '@config/i18n/messages';
+import logger from '@utils/logger';
 import httpStatus from '@constants/httpStatus';
 import DatabaseConnection from '@database/databaseConnection';
+import { getDatabaseMessages } from '@config/i18n/messages';
 import { ErrorHandler } from '@middlewares/errorHandler';
-import logger from '@utils/logger';
+
 
 export default class SignalHandler {
     private static async handleExit(signal: string, message: string) {
@@ -21,34 +22,38 @@ export default class SignalHandler {
     }
 
     public static handleNodemonRestarts(): void {
-        process.once('SIGUSR2', async () => {
-            try {
-                await this.handleExit('SIGUSR2', getDatabaseMessages.connectionShutdown.nodemonRestart);
-            } catch (error) {
-                if (error instanceof ErrorHandler) {
-                    logger.error(new ErrorHandler(httpStatus.INTERNAL_SERVER_ERROR, getDatabaseMessages.connectionShutdown.nodemonRestartError(error.message), error.stack));
-                } else {
-                    logger.error(new ErrorHandler(httpStatus.INTERNAL_SERVER_ERROR, getDatabaseMessages.unknownDatabaseError));
+        process.once('SIGUSR2', () => {
+            (async () => {
+                try {
+                    await this.handleExit('SIGUSR2', getDatabaseMessages.connectionShutdown.nodemonRestart);
+                } catch (error) {
+                    if (error instanceof ErrorHandler) {
+                        logger.error(new ErrorHandler(httpStatus.INTERNAL_SERVER_ERROR, getDatabaseMessages.connectionShutdown.nodemonRestartError(error.message), error.stack));
+                    } else {
+                        logger.error(new ErrorHandler(httpStatus.INTERNAL_SERVER_ERROR, getDatabaseMessages.unknownDatabaseError));
+                    }
+                    
+                    process.exit(1);
                 }
-                
-                process.exit(1);
-            }
+            })();
         });
     }
 
     public static handleAppTermination(): void {
-        process.on('SIGINT', async () => {
-            try {
-                await this.handleExit('SIGINT', getDatabaseMessages.connectionShutdown.applicationTermination);
-            } catch (error) {
-                if (error instanceof ErrorHandler) {
-                    logger.error(new ErrorHandler(httpStatus.INTERNAL_SERVER_ERROR, getDatabaseMessages.connectionShutdown.applicationTerminationError(error.message), error.stack));
-                } else {
-                    logger.error(new ErrorHandler(httpStatus.INTERNAL_SERVER_ERROR, getDatabaseMessages.unknownDatabaseError));
-                }
+        process.on('SIGINT', () => {
+            (async () => {
+                try {
+                    await this.handleExit('SIGINT', getDatabaseMessages.connectionShutdown.applicationTermination);
+                } catch (error) {
+                    if (error instanceof ErrorHandler) {
+                        logger.error(new ErrorHandler(httpStatus.INTERNAL_SERVER_ERROR, getDatabaseMessages.connectionShutdown.applicationTerminationError(error.message), error.stack));
+                    } else {
+                        logger.error(new ErrorHandler(httpStatus.INTERNAL_SERVER_ERROR, getDatabaseMessages.unknownDatabaseError));
+                    }
 
-                process.exit(1);
-            }
+                    process.exit(1);
+                }
+            })();
         });
     }
 }
